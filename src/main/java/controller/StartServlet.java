3522f4af440e645fa014fdbe0cd05e2c6f8115df
Derskit <obsaga14@gmail.com>
2026-03-29 13:01:50 +0500

    Initial commit

---
+++ src/main/java/controller/StartServlet.java
@@ -0,0 +1,25 @@
+package controller;
+
+import model.GameState;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+@WebServlet(name = "InitServlet", value = "/start")
+public class InitServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // Создание новой сессии
+        HttpSession currentSession = req.getSession(true);
+
+        // Перенаправление запроса на страницу start.jsp через сервер
+        getServletContext().getRequestDispatcher("/jsp/start.jsp").forward(req, resp);
+    }
+}
